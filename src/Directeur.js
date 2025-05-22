41eda1ec5468d56497e209ab83483463bef354cc
Egregorion <gregory.fourmaux@gmail.com>
2025-05-22 16:27:18 +0200

    corrections et commentaires de la classe Directeur

--- src/Directeur.js
+++ src/Directeur.js
@@ -1,9 +1,10 @@
 import { Employe } from "./Employe.js";
 
-export class Directeur extends Employe {
-    calculPrime() {
-        const primeAnnuelle = this.salaire * 0.07 //12.5
-        const primeAnciennete = this.salaire * 0.03 * this.calculAnciennete() //150
+export class Directeur extends Employe { //on fait hériter la classe Directeur des méthodes et propriétés de Employe grâce à "extends". Il s'agit du principe d'héritage
+    
+    calculPrime() { //on redéfinie la méthode calculPrime, ainsi elle va remplacer celle de sa classe parente
+        const primeAnnuelle = this.salaire * 0.07 // on passe de 5% à 7%
+        const primeAnciennete = this.salaire * 0.03 * this.calculAnciennete() // de 2% à 3%
         const primeTotale = primeAnnuelle + primeAnciennete
         return primeTotale
     }
